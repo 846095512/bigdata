d74c80091de9dfc036cfa835cbb094cdf1e1c69d
zheng <846095512@qq.com>
2024-11-27 09:24:02 +0800

    update

---
+++ install_script/system_config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from install_script.commons import exec_shell_command
+
+
+def init_os_conf():
+    # 交换分区
+    exec_shell_command("swapoff -a")
+    exec_shell_command("sed -i '/swap/s/^/#/' /etc/fstab")
+
+    # cpu 透明大页关闭
+    exec_shell_command("echo never > /sys/kernel/mm/transparent_hugepage/enabled")
+    exec_shell_command("echo never > /sys/kernel/mm/transparent_hugepage/defrag")
+
+    # 系统文件描述符 进程限制
+    exec_shell_command("echo '* soft nofile 1000000'  > /etc/security/limits.conf")
+    exec_shell_command("echo '* hard nofile 1000000'  > /etc/security/limits.conf")
+    exec_shell_command("echo '* soft nproc 1000000'  > /etc/security/limits.conf")
+    exec_shell_command("echo '* hard nproc 1000000'  > /etc/security/limits.conf")
+
+    # 防火墙
+    exec_shell_command("systemctl stop firewalld.service")
+    exec_shell_command("systemctl disable firewalld.service")
+
+    # cpu性能调度模式
+    exec_shell_command("echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
