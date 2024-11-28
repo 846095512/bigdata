8dfdc3d2dda03b68ea175110d942210914667750
zheng <846095512@qq.com>
2024-11-28 15:13:59 +0800

    update

--- install_script/install_kafka.py
+++ install_script/install_kafka.py
@@ -4,6 +4,53 @@ from commons import *
 
 def install_kafka():
     kafka_conf_template="""
-    
+# 基础配置
+broker.id={{ broker_id }}
+{% if kraft_enable == "true" %}
+process.roles=broker,controller
+listeners=PLAINTEXT://{{ broker_list }},CONTROLLER://{{ controller_list }}
+advertised.listeners=PLAINTEXT://{{ local_ip }},
+controller.quorum.voters={{ broker_id }}@{{ local_ip }}:9093 # 逗号分隔
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT,CONTROLLER:PLAINTEXT
+{% else %}
+listeners=PLAINTEXT://{{ broker_list }}
+advertised.listeners=PLAINTEXT://{{ local_ip }}
+zookeeper.connect={{ zk_addr }}
+zookeeper.connection.timeout.ms=3000
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT
+{% endif %}
+# 分区
+num.partitions={{ partitions_default }}
+min.insync.replicas={{ partitions_default }}
+default.replication.factor={{ partitions_default }}
 
+# 日志存储
+log.dirs={{ kafka_home_dir }}/message
+log.retention.hours=3
+log.segment.bytes=1073741824
+log.retention.bytes=10737418240
+log.flush.interval.ms=10000
+log.cleanup.policy=delete
+log.retention.check.interval.ms=300000
+log.flush.interval.messages=10000
+unclean.leader.election.enable=false
+
+# 线程数
+num.network.threads=3
+num.io.threads=8
+
+# 元数据刷新
+metadata.max.age.ms=300000
+
+# 副本同步
+replica.fetch.min.bytes=524288
+replica.fetch.max.bytes=2097152
+replica.lag.time.max.ms=10000
+
+# 最大消息大小
+message.max.bytes=20971520
+
+# 受控关闭
+controlled.shutdown.enable=true
+controlled.shutdown.max.retries=3
 """
