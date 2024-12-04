d384451c083f2fb8945d94223ac2f039a6595ffe
清清清静 <846095512@qq.com>
2024-12-04 19:39:45 +0800

    update

--- install_script/commons.py
+++ install_script/commons.py
@@ -246,6 +246,7 @@ def configure_environment(app, app_home):
                         f3.write(f"export {app}={app_home}\n")
                         f3.write(f"export PATH=$PATH:${app}/bin\n")
         res = exec_shell_command(f"source {env_file} && env")
+        print(res)
         env_lines = res.splitlines()
         os.environ.update(line.split('=', 1) for line in env_lines if "=" in line)
         print("environment configure success")
