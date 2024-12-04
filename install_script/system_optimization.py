ffd17d5af50d27bad72fd3d80d21ad7d0fe8a4c7
zheng <846095512@qq.com>
2024-12-04 09:14:27 +0800

    update

--- install_script/system_optimization.py
+++ install_script/system_optimization.py
@@ -24,10 +24,6 @@ def init_os_conf():
     exec_shell_command("echo '* soft nproc 1000000'  >> /etc/security/limits.conf")
     exec_shell_command("echo '* hard nproc 1000000'  >> /etc/security/limits.conf")
 
-    # 防火墙
-    exec_shell_command("systemctl stop firewalld")
-    exec_shell_command("systemctl disable firewalld")
-
     # cpu性能调度模式
     exec_shell_command("echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor")
 
@@ -36,6 +32,17 @@ def init_os_conf():
     exec_shell_command("modprobe ip_vs_rr")
     exec_shell_command("modprobe ip_vs_wrr")
     exec_shell_command("modprobe ip_vs_sh")
+
+    exec_shell_command("sed -i 's|X11Forwarding yes|X11Forwarding no|g' /etc/ssh/sshd_config")
+    exec_shell_command("systemctl restart sshd")
+
+    if get_os_name() == "ubuntu" or get_os_name() == "debian":
+        exec_shell_command("ufw disable")
+        exec_shell_command("echo 'LANG=en_GB.UTF-8' >> /etc/default/locale")
+    elif get_os_name() == "centos" or get_os_name() == "redhat":
+        exec_shell_command("systemctl stop firewalld")
+        exec_shell_command("systemctl disable firewalld")
+        exec_shell_command("echo 'LANG=en_GB.UTF-8' >> /etc/locale.conf")
 
 
 if __name__ == '__main__':
