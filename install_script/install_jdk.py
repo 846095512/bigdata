22a4ada3d2bf7dbb13f225944a55001b5188e8b5
清清清静 <846095512@qq.com>
2024-12-04 19:37:47 +0800

    update

--- install_script/install_jdk.py
+++ install_script/install_jdk.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from commons import *
 
+app_home = os.path.join(get_app_home_dir(), module_name)
+
 
 def install_jdk():
-    app_home = os.path.join(get_app_home_dir(), module_name)
-    delete_dir(app_home)
     set_permissions(app_home)
     configure_environment("JAVA_HOME", app_home)
     exec_shell_command("java -version", "Jdk Installation", output=True)
 
 
 if __name__ == '__main__':
+    delete_dir(app_home)
     unzip_package()
     install_jdk()
