d35da2df312ac8200e54a571f3b54431daeee0f1
清清清静 <846095512@qq.com>
2024-12-07 01:45:33 +0800

    update

--- install_script/install_chrony.py
+++ install_script/install_chrony.py
@@ -6,9 +6,11 @@ from commons import *
 def install_chrony():
     ntp_conf = os.path.join(app_home_dir, "conf", 'ntp.conf')
     cird_notation = params_dict["cird.notation"]
+    chrony_server_ip = params_dict["chrony.server.ip"]
     generate_config_file(template_str=chrony_conf_template,
                          conf_file=ntp_conf,
-                         cired_notation=cird_notation)
+                         cired_notation=cird_notation,
+                         chrony_server_ip=chrony_server_ip)
     exec_shell_command(f"{app_home_dir}/sbin/chronyd -f {app_home_dir}/conf/chrony.conf",
                        "chrony start", output=True)
     print("chrony installation completed")
@@ -19,7 +21,7 @@ if __name__ == '__main__':
 server ntp1.aliyun.com iburst
 server ntp2.aliyun.com iburst
 server ntp3.aliyun.com iburst
-local stratum 10
+server {{ chrony_server_ip }} stratum 10
 driftfile {{ chrony_home_dir }}/chrony.drift
 logdir {{ chrony_home_dir }}/log
 log measurements statistics tracking
